556ef58c29f77af4da3db66075f8d2d9b3861039
Ajay <ajaykmrbel@gmail.com>
2025-10-10 13:50:02 +0530

    Added Backend file

---
+++ ChatBotBackend/src/main/java/com/chatbot/ChatBotBackend/ChatBotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.ChatBotBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotBackendApplication.class, args);
+	}
+
+}
---
+++ ChatBotBackend/src/test/java/com/chatbot/ChatBotBackend/ChatBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.ChatBotBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
