852906295ca77544ab13b03e9c176f6d14b686e9
Ajay <ajaykmrbel@gmail.com>
2025-10-11 14:06:29 +0530

    Successfully Added DepseekAi Through Hugging Face

--- ChatBotBackend/src/main/java/com/chatbot/ChatBotBackend/dto/ChatRequest.java
+++ ChatBotBackend/src/main/java/com/chatbot/ChatBotBackend/dto/ChatRequest.java
@@ -5,4 +5,10 @@ import lombok.Data;
 @Data
 public class ChatRequest {
     private String message;
+
+    public ChatRequest() {}
+
+    public ChatRequest(String message) {
+        this.message = message;
+    }
 }
